025180f7d3cc4753a6d57c2e37a8bb9fcbce01b2
bsherilyn18 <117791917+bsherilyn18@users.noreply.github.com>
2022-11-09 14:11:09 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package javacode;
+import java.util.Scanner;
+public class Main {
+
+  public static void main (String[]args){
+
+	Scanner assignment = new Scanner (System.in);
+
+   int she = 0, sum = 0, grades = 0, i = 0;
+
+do{
+	System.out.println ("Do you want to enter a grade?");
+	System.out.println ("Press 1 == YES");
+	System.out.println ("Press 2 == NO");
+   	she = assignment.nextInt();
+
+if ( she == 1){
+	System.out.println ("You chose YES");
+	System.out.println ("Please enter a grade:");
+	grades = assignment.nextInt();
+	sum += grades;
+	i++;
+}else{
+	System.out.println ("You chose NO"); }
+}while (she == 1);
+
+	sum = sum / i;
+
+	System.out.print ("Total average is: " +sum);
+
+	}
+}
